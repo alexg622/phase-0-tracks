10f872ac15d9763172c738f063a2ed8a31ff2dfc
Alex Gonzalez <Alex-Crystal@Alex.local>
2016-11-05 00:09:20 -0700

    initial commit

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -1,3 +1,5 @@
+#1
+
 def array_search(arr, number)
 	if number == "#{number}".to_i
 		puts arr["#{number}".to_i]
@@ -8,10 +10,27 @@ end
 
 p array_search(arr, 0)
 
+#2
 
-puts "give me a number"
-name = gets.chomp.to_i 
-if name == "#{name}".to_i
-puts ("#{name}".to_i - 1) + ("#{name}".to_i - 2)
-end
- 	
+def fib(n)
+
+i = 2 
+terms = [0, 1]
+until i > n - 1 
+terms.push(terms[i-1] + terms[i-2])
+i += 1 
+end 
+terms 
+end 
+
+fib(100)
+
+#3
+#Wrapping my brain around how sorts works is pretty tough. I feel like 
+#there's more methods and things I need to be able to learn before 
+#being able to create a method like that. If it wasn't ruby my 
+#psuedocode would be take the smallest number and put it first in the 
+#array, then the next smallest after that and put that next
+#Keep doing that until you reach the amount of numbers given
+x = [3, 2, 4, 6]
+x.sort
