0c1cc2b29bce0d807c2fbad6d9f25a6c30cb2c42
Alex Gonzalez <Alex-Crystal@Alex.local>
2016-11-06 15:41:35 -0800

    initial commit

--- ruby/calculator.rb
+++ ruby/calculator.rb
@@ -35,3 +35,29 @@ p x/y
 end
 end 
 calculate2
+
+def calculate2 
+x = 0 
+until x == "done" 
+	puts "give me a calculation you would like me to do for you"
+	puts "what is your first number?"
+	x = gets.chomp
+if x == "done" 
+	break
+end 
+	puts "what is your second number"
+	y = gets.chomp
+	puts "what would you like me to do with this"
+	b = gets.chomp
+if b == "+"
+	p x.to_i+y.to_i 
+elsif b == "-"
+p x.to_i-y.to_i
+elsif b == "*"
+p x.to_i*y.to_i 
+elsif b == "/"
+p x.to_i/y.to_i 
+end
+end 
+end 
+calculate2
