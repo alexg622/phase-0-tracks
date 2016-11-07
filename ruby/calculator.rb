5115eb9ffbdc8b78ab147076dec84b06d46fdcf7
Alex Gonzalez <Alex-Crystal@Alex.local>
2016-11-06 16:47:58 -0800

    commit

--- ruby/calculator.rb
+++ ruby/calculator.rb
@@ -6,63 +6,6 @@ p	x/y
 end 
 
 def calculate2 
-	puts "give me a calculation you would like me to do for you"
-	puts "what is your first number?"
-	x  = gets.chomp.to_i
-	puts "what is your second number"
-	y = gets.chomp.to_i 
-	puts "what would you like me to do with this"
-	b = gets.chomp 
-	p x b y 
-end 
-
-def calculate2 
-	puts "give me a calculation you would like me to do for you"
-	puts "what is your first number?"
-	x = gets.chomp.to_i
-	puts "what is your second number"
-	y = gets.chomp.to_i 
-	puts "what would you like me to do with this"
-	b = gets.chomp
-if b == "+"
-	p x+y 
-elsif b == "-"
-p x-y 
-elsif b == "*"
-p x*y 
-elsif b == "/"
-p x/y 
-end
-end 
-calculate2
-
-def calculate2 
-x = 0 
-until x == "done" 
-	puts "give me a calculation you would like me to do for you"
-	puts "what is your first number?"
-	x = gets.chomp
-if x == "done" 
-	break
-end 
-	puts "what is your second number"
-	y = gets.chomp
-	puts "what would you like me to do with this"
-	b = gets.chomp
-if b == "+"
-	p x.to_i+y.to_i 
-elsif b == "-"
-p x.to_i-y.to_i
-elsif b == "*"
-p x.to_i*y.to_i 
-elsif b == "/"
-p x.to_i/y.to_i 
-end
-end 
-end 
-calculate2
-
-def calculate2 
 terms = []
 x = 0 
 until x == "done" 
@@ -104,3 +47,5 @@ puts "you have #{z} calculations performed:"
 terms.map! do |v| 
 	puts v 
 end 
+end
+calculate2
