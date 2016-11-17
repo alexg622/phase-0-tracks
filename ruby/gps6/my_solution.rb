45339dd012e9489fa590ffb6aa9014c5610df4a6
aleg622 <alex.gm62288@gmail.com>
2016-11-16 18:02:06 -0800

    completed

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -4,11 +4,13 @@
 # We spent [#] hours on this challenge.
 
 # EXPLANATION OF require_relative
-#
-#
+# Require_relative links files together, getting data from another file.
+# Require_relative links a file in the same directory. Require links a file from a different location. Require can also pull in ruby files, gems, etc.
 require_relative 'state_data'
+require 'pry'
 
 class VirusPredictor
+  # We are initializing a new instance of our VirusPredictor class with input of state_of_origin, population_density, and population.
 
   def initialize(state_of_origin, population_density, population)
     @state = state_of_origin
@@ -16,14 +18,17 @@ class VirusPredictor
     @population_density = population_density
   end
 
+# Virus_effects show us the output of the information from a particular state. It shows us the predicted deaths and speed of spread.
+
   def virus_effects
-    predicted_deaths(@population_density, @population, @state)
-    speed_of_spread(@population_density, @state)
+    predicted_deaths
+    speed_of_spread
   end
 
   private
+  # This method predicts the deaths based on population density and uses the floor method to round down to the nearest whole number. It then prints 
 
-  def predicted_deaths(population_density, population, state)
+  def predicted_deaths
     # predicted deaths is solely based on population density
     if @population_density >= 200
       number_of_deaths = (@population * 0.4).floor
@@ -41,7 +46,9 @@ class VirusPredictor
 
   end
 
-  def speed_of_spread(population_density, state) #in months
+  def speed_of_spread 
+  # This method is predicting the speed of spread based on the state's population density measured in months. Based on the population density, it tells us how many months it will take to spread across the state.
+ #in months
     # We are still perfecting our formula here. The speed is also affected
     # by additional factors we haven't added into this functionality.
     speed = 0.0
@@ -81,6 +88,18 @@ california.virus_effects
 
 alaska = VirusPredictor.new("Alaska", STATE_DATA["Alaska"][:population_density], STATE_DATA["Alaska"][:population])
 alaska.virus_effects
+
+STATE_DATA.each do |state_name, numbers| 
+ # binding.pry
+  state = VirusPredictor.new(state_name, numbers[:population_density], numbers[:population]).virus_effects 
+end 
+
+# the hash syntax that is used in state_data allows us to use strings, while the other hash syntax does not. 
+# require_relative can require a file in the same directory or folder while require can require a file from a different directory as long as you provide the proper path.
+# In this gps there we used .each to iterate through the hash. 
+# That it wasn't necassary to repeat the variables. Its hard to catch them sometimes when everything is all named the same thing. 
+# This challenged helped to give me practice with hashes, and iterating through them to produce data for everything in a hash than just a few specific instances.  
+
 
 
 #=======================================================================
