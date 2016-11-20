52889b88f1516ea061bd06b290df49d9c1d0a2a4
aleg622 <alex.gm62288@gmail.com>
2016-11-20 00:42:19 -0800

    copied in pseudocode

--- js/algos.js
+++ js/algos.js
@@ -79,9 +79,24 @@ longest_word("hi", "this", "is", "my really", "really really", "really", "seven"
 longest_word("hi this is really the longest phrase", "this", "is", "my really", "really really", "really", "seven", "eight", "nine nine nine", "ten, ten ten ten ten");
 generate(10) 
 
+//this loop only returns the last part of the loop. Like if I wanted it to print l it would only print 10 instead of 1-10. I don't know why. 
 for(var l=0; l<10; l+=1); {
-  longest_word(generate(5));
+  longest_word(generate(5)); //I don't know why my longest_word function won't work on the generate function. 
 }
+
+//code
+
+//This funtion will take the longest word or phrase from an array 
+//of four words or phrases and output the longest word or phrase. It will do this 
+//by looping through each word in the phrase and only printing it if the phrase before it is longer than the new one. 
+
+//This function will take a hash and compare the values through its keys. 
+//If the values are the same it will print true, and if the values are different it will print false. 
+//This function will take two keys and values, and compare them with and elseif statement to determine whether they are = or not and should print true or false. 
+
+//The final function will take a number and return random words of a length from 0-10 that number of times. 
+//I will need a variable holding the alphabet, and then use a random letter of the alphabet to generate a word from length 1-10, 
+//and use a loop to generate x amounts of that word. 
 
 
 
